cd042647fbd393c2b5be524f0a826db93a412404
hyperbotauthor <hyperchessbotauthor@gmail.com>
2021-11-07 17:39:17 +0000

    Composition API slots

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  <HelloWorld>
+    <div>Div 1</div>
+    <div>Div 2</div>
+  </HelloWorld>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
