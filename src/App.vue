9361dba32e6eba3e4b2544b8de98e1270ef12923
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-11-07 18:53:02 +0100

    Slots used from other component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <!--<img alt="Vue logo" src="./assets/logo.png">-->
+  Works with plain slots
   <HelloWorld>
     <div>Div 1</div>
     <div>Div 2</div>
   </HelloWorld>
+  Triggers warning when slots are used from other component
+  <Composite> </Composite>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
+import Composite from "./components/Composite";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Composite,
+  },
+};
 </script>
 
 <style>
