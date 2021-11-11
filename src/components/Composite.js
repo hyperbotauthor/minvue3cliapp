2465a1ef949092128eae4d2b05796adb78663ec1
hyperbotauthor <73212952+hyperbotauthor@users.noreply.github.com>
2021-11-11 15:22:21 +0100

    Remove unnecessary args

--- src/components/Composite.js
+++ src/components/Composite.js
@@ -3,7 +3,7 @@ import { defineComponent, h } from "vue";
 import HelloWorld from "./HelloWorld";
 
 export default defineComponent({
-  setup(props, { slots }) {
+  setup() {
     return () =>
       h(HelloWorld, {}, [h("div", {}, ["Div 1"]), h("div", {}, ["Div 2"])]);
   }
